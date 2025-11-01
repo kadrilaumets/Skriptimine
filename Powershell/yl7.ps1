7358acd792a877956a25d0ba68391136aeb15436
kadrilaumets <kadrilaumets@gmail.com>
2025-11-01 09:27:37 +0200

    powershell upload

---
+++ Powershell/yl7.ps1
@@ -0,0 +1,10 @@
+﻿cls
+$pc=hostname
+$d=@(get-wmiobject win32_logicaldisk -filter "DriveType=3")
+Write-Host "Arvuti:"$pc
+Write-Host "Kettaid kokku:"$($d.Count)
+foreach($x in $d){
+$p=[math]::Round(($x.FreeSpace/$x.Size)*100,2)
+Write-Host "$($x.DeviceID) - $p%"
+if($p -lt 50){Write-Host "Hakkab täis saama"}
+}
